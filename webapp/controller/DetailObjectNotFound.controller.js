6e5d4a6f94663902e68a18b4eddfdaf12552c3a5
FioriDeveloper20 <FioriDeveloper20@yahoo.com>
2021-02-26 14:16:39 +0000

    Initial Commit By Developer 20
    Signed-off-by: FioriDeveloper20 <FioriDeveloper20@yahoo.com>

---
+++ webapp/controller/DetailObjectNotFound.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+	"./BaseController"
+], function (BaseController) {
+	"use strict";
+
+	return BaseController.extend("learninghub.learningroom.btp.flightanalysis1.controller.DetailObjectNotFound", {});
+});
